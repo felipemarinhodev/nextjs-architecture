20b5460b936cbe87bff23c35e08440400a89250e
felipemarinhodev <felipmarinho@gmail.com>
2022-07-15 06:58:11 -0700

    a2.2 - yarn workspaces and utils packages

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { sum } from "@felipemarinhodev/utils/math/sum";
 
 export default function HomeScreen() {
   return (
     <main>
       <h1>Home</h1>
+      <p>
+        Importando modulo local: @felipemarinhodev/utils/math/sum sum(2,2):{" "}
+        {sum(2, 2)}
+      </p>
     </main>
   );
 }

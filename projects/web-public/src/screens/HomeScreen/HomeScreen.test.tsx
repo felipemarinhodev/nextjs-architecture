663d09bdb349d73ab9197a8e9557dbe6f3a3ba42
felipemarinhodev <felipmarinho@gmail.com>
2022-07-21 10:48:53 -0700

    a4.6 - example to use custom wrapper

--- projects/web-public/src/screens/HomeScreen/HomeScreen.test.tsx
+++ projects/web-public/src/screens/HomeScreen/HomeScreen.test.tsx
@@ -1,6 +1,16 @@
 import { customRender } from "@felipemarinhodev/test-commons/react-testing-library";
 import { HomeScreen } from "./HomeScreen";
 
+/**
+ * If is necessary to use some provider wrapping the component,
+ * it should use this custom wrapper.
+ */
+// function CustomWrapper({ children }) {
+//   console.log("CustomWrapper Wrapper");
+//   return <section>{children}</section>;
+// }
+// const render = customRender(CustomWrapper);
+
 const render = customRender();
 
 describe("<HomeScreen />", () => {
